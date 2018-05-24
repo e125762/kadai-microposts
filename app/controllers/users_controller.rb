83c67425a69d22713b14f127bdbff54c0b34a6f4
keito.gibo <e125762@ie.u-ryukyu.ac.jp>
2018-05-24 08:44:34 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
+
   def index
     @users = User.all.page(params[:page])
   end
