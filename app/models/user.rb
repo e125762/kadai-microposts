7a0f40ffd9a1b01108292e69fd5bb00ae391a1fd
keito.gibo <e125762@ie.u-ryukyu.ac.jp>
2018-05-30 07:23:42 +0000

    fix dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,14 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
 
-  has_many :microposts, dependent: :destroy
+  has_many :microposts
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
-  has_many :likes, dependent: :destroy
-  has_many :liked_microposts, through: :likes, source: :micropost
-#  has_many :microposts, through: :likes
+  has_many :likes
+  has_many :liked_microposts, through: :likes, source: :micropost 
 
   def follow(other_user)
     unless self == other_user
